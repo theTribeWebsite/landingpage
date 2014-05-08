552faa166d5700f7b0cdbb8d82e3f1586d82f22a
MasonPelt <mason@masonpelt.com>
2014-05-08 18:32:34 -0500

    A lot

--- js/custom.js
+++ js/custom.js
@@ -76,3 +76,22 @@ function showElem(){
 	}
 
 });
+
+/* =================================
+===  MAILCHIMP                 ====
+=================================== */
+
+$('.mailchimp').ajaxChimp({
+    callback: mailchimpCallback,
+    url: "http://thetribeapp.us8.list-manage1.com/subscribe/post?u=629af7f31dc33e62c3cb8354d&amp;id=484293bbb6" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".  
+});
+
+function mailchimpCallback(resp) {
+     if (resp.result === 'success') {
+        $('.subscription-success').html('<i class="icon_check_alt2"></i><br/>' + resp.msg).fadeIn(1000);
+        $('.subscription-error').fadeOut(500);
+        
+    } else if(resp.result === 'error') {
+        $('.subscription-error').html('<i class="icon_close_alt2"></i><br/>' + resp.msg).fadeIn(1000);
+    }  
+}
